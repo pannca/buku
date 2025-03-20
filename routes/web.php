22011a942bd49c0cae860b81518b539bd71be6e2
pannca <pancaprasetia@smkwikrama.sch.id>
2025-03-20 17:49:21 +0700

    fix : memperbaiki route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('tes');
+// });
+
+Route::get('/', [BookController::class, 'index'])->name('buku.index');
+Route::get('/buku/create', [BookController::class, 'create'])->name('buku.create');
+Route::post('/buku', [BookController::class, 'store'])->name('buku.store');
+Route::get('/buku/{id}/edit', [BookController::class, 'edit'])->name('buku.edit');
+Route::put('/buku/{id}', [BookController::class, 'update'])->name('buku.update');
+Route::delete('/buku/{id}', [BookController::class, 'destroy'])->name('buku.destroy');
+
+Route::prefix('categories')->name('categories.')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('index'); // Tanpa '/categories'
+    Route::get('/create', [CategoryController::class, 'create'])->name('create'); // Gunakan get, bukan create()
+    Route::post('/', [CategoryController::class, 'store'])->name('store'); // Tanpa '/categories'
+    Route::get('/{id}/edit', [CategoryController::class, 'edit'])->name('edit'); // Tanpa '/categories'
+    Route::put('/{id}', [CategoryController::class, 'update'])->name('update'); // Tanpa '/categories'
+    Route::delete('/{id}', [CategoryController::class, 'destroy'])->name('destroy'); // Tanpa '/categories'
 });
